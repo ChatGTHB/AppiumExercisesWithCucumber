b922aad3ae1e3581cd6c04efc026aa3416f70544
ChatGTHB <cihat.kose@hotmail.com>
2023-12-03 00:52:31 +0100

    Added AmazonWebPage.java

---
+++ src/test/java/page/AmazonWebPage.java
@@ -0,0 +1,23 @@
+package page;
+
+import org.openqa.selenium.support.PageFactory;
+import utils.Hooks;
+
+
+public class AmazonWebPage extends Hooks {
+    public AmazonWebPage() {
+        PageFactory.initElements(androidDriver, this);
+    }
+
+    public String searchTextBoxJS(String key) {
+        return "document.querySelector(\"#twotabsearchtextbox\").value='" + key + "';";
+    }
+
+    public String clickSearchBoxJS() {
+        return "document.querySelector(\"#nav-search-submit-button\").click();";
+    }
+
+    public void executeElement(String element) {
+        androidDriver.executeScript(element);
+    }
+}
