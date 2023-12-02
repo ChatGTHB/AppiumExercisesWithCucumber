da45efe4bad1a810beb1f9315766debc12e577c9
ChatGTHB <cihat.kose@hotmail.com>
2023-12-03 00:47:33 +0100

    Added SauceDemoStep.java

---
+++ src/test/java/steps/SauceDemoStep.java
@@ -0,0 +1,32 @@
+package steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import page.SauceDemoPage;
+import utils.Hooks;
+
+public class SauceDemoStep extends Hooks {
+    SauceDemoPage sauceDemoPage = new SauceDemoPage(androidDriver);
+
+    @Given("I have opened Souce Demo Web Page")
+    public void i_have_opened_souce_demo_web_page() {
+        androidDriver.get("https://www.saucedemo.com/");
+    }
+
+    @When("I enter {string} and {string}")
+    public void i_enter_and(String username, String password) {
+        sauceDemoPage.usernameTextBoxJS(username);
+        sauceDemoPage.passwordTextBoxJS(password);
+    }
+
+    @Then("I click on the login button")
+    public void i_click_on_the_login_button() {
+        sauceDemoPage.clickLogin();
+    }
+
+    @Then("I should be able to see header text")
+    public void i_should_be_able_to_see_header_text() {
+    }
+}
